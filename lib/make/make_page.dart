16b472e40d2cf69727aad1e6d66f6dde478048c9
kei1115 <keifukuda1115@gmail.com>
2021-10-10 05:09:12 +0900

    Add TextFields

--- lib/make/make_page.dart
+++ lib/make/make_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ore_kota/make/component/choice.dart';
 import 'package:ore_kota/make/component/choose_people_page.dart';
 
-class MakePage extends StatelessWidget {
+class MakePage extends StatefulWidget {
   const MakePage({Key? key}) : super(key: key);
+
+  @override
+  _MakePageState createState() => _MakePageState();
+}
+
+class _MakePageState extends State<MakePage> {
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +22,25 @@ class MakePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'Make',
+            TextField(
+              controller: myController1,
+              decoration: InputDecoration(
+                labelText: "タイトルを入力してください",
+              ),
+              textAlignVertical: TextAlignVertical(y: -1),
             ),
+            TextField(
+              controller: myController2,
+              decoration: InputDecoration(
+                labelText: "詳しい内容を入力してください",
+              ),
+              textAlignVertical: TextAlignVertical.bottom,
+            ),
+            Choice(),
+            Choice(),
+            Choice(),
           ],
         ),
       ),
