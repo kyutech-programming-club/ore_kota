0d1a1c5fc811c93b818b61c90a08d3c4c9210669
light-ai <subarasiisekaiyuu.seraph@gmail.com>
2021-10-10 00:10:44 +0900

    Add approute

--- lib/choose_people_page.dart
+++ lib/choose_people_page.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChoosePeoplePage extends StatelessWidget {
-  const ChoosePeoplePage({Key? key}) : super(key: key);
+class ChoosePeoplePage extends StatefulWidget {
+  @override
+  _ChoosePeoplePageState createState() => _ChoosePeoplePageState();
+}
+
+class _ChoosePeoplePageState extends State<ChoosePeoplePage> {
+  static const _alignments = [
+    Alignment.topLeft,
+    Alignment.topRight,
+    Alignment.bottomRight,
+    Alignment.bottomLeft,
+  ];
+
+  var _index = 0;
+  AlignmentGeometry get _alignment => _alignments[_index % _alignments.length];
 
   @override
+  double _width = 0;
+
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white70
+    final Size size = MediaQuery.of(context).size;
+    void _onTap() => setState(() => _width = size.height*0.9);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AnimatedContainer'),
       ),
-      child: Text("choose"),
+      body: Align(
+        alignment: Alignment(0, 1),
+          child: AnimatedContainer(
+            width: size.width*0.8,
+            height: _width,
+            duration: Duration(milliseconds: 500),
+            color: Colors.orange,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: _onTap),
     );
   }
 }
+
