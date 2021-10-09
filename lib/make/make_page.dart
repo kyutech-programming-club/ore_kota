05d0da8d48cef2ad88472ab7af44b3956aff7a23
light-ai <subarasiisekaiyuu.seraph@gmail.com>
2021-10-10 07:09:06 +0900

    Add insert data into firebase function

--- lib/make/make_page.dart
+++ lib/make/make_page.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ore_kota/common/custom_button.dart';
+import 'package:ore_kota/firebase/googleSignInMethod.dart';
 import 'package:ore_kota/make/component/choice.dart';
 import 'package:ore_kota/make/component/choose_people_page.dart';
 
@@ -14,9 +16,27 @@ class MakePage extends StatefulWidget {
 class _MakePageState extends State<MakePage> {
   final myController1 = TextEditingController();
   final myController2 = TextEditingController();
+  final myController3 = TextEditingController();
+  final myController4 = TextEditingController();
+  final myController5 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+
+    void onTap() async{
+      await FirebaseFirestore.instance
+          .collection('questions') // コレクションID
+          .doc() // ここは空欄だと自動でIDが付く
+          .set({
+        'title' : myController1.text,
+        'description' : myController2.text,
+        'user_id' : GoogleSignInMethod().currentUser!.uid,
+        'choice.1' : myController3.text,
+        'choice.2' : myController4.text,
+        'choice.3' : myController5.text,
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("makePage"),
@@ -40,14 +60,29 @@ class _MakePageState extends State<MakePage> {
               ),
               textAlignVertical: TextAlignVertical.bottom,
             ),
-            Choice(),
-            Choice(),
-            Choice(),
+            TextField(
+              controller: myController3,
+              decoration: InputDecoration(
+                labelText: "選択肢を入力",
+              ),
+            ),
+            TextField(
+              controller: myController4,
+              decoration: InputDecoration(
+                labelText: "選択肢を入力",
+              ),
+            ),
+            TextField(
+              controller: myController5,
+              decoration: InputDecoration(
+                labelText: "選択肢を入力",
+              ),
+            ),
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  CustomButton(text: "作成"),
+                  CustomButton(text: "作成", onTap: onTap,),
                   SizedBox(height: 16),
                 ],
               ),
