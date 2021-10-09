9a705dd1828b054252b9adf8e895228f4c60ecf5
light-ai <subarasiisekaiyuu.seraph@gmail.com>
2021-10-10 07:09:37 +0900

    Add get data from firebase function

--- lib/root/root_page.dart
+++ lib/root/root_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ore_kota/firebase/googleSignInMethod.dart';
 import 'package:ore_kota/home/home_page.dart';
-import 'package:ore_kota/signIn/signIn_page.dart';
+import 'package:ore_kota/main.dart';
 
 class RootPage extends StatelessWidget {
 
@@ -10,6 +10,6 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     User? user = GoogleSignInMethod().currentUser;
 
-    return user == null ? SignInPage() : HomePage();
+    return user == null ? MyHomePage(title: "") : HomePage();
   }
 }
