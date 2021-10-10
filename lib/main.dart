dd3ada4cdd41ecc0a6074d9f92efd348473390cc
syu-kwsk <wataru1165kwsk@gmail.com>
2021-10-10 10:47:11 +0900

    Remove useless import

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:ore_kota/firebase/userInfo.dart';
-import 'package:ore_kota/home/home_page.dart';
 import 'package:ore_kota/root/root_page.dart';
 
-import 'firebase/googleSignInMethod.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
