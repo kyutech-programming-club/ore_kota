8972764f41140b808f9e37840de1a0198fa468dd
kei1115 <keifukuda1115@gmail.com>
2021-10-10 10:39:47 +0900

    Add Stack

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ore_kota/common/custom_button.dart';
 import 'package:ore_kota/home/component/question.dart';
 import 'package:ore_kota/make/make_page.dart';
-import 'package:ore_kota/answer/answer_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isShow = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,21 +21,47 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 16, right: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Question(
-                questionName: "日程の調整",
-                peopleNumber: 3,
-                description: "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Question(
+                    questionName: "日程の調整",
+                    peopleNumber: 3,
+                    description: "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("aaa");
+                  },
+                  child: Text("dialog"),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AnswerPage()),
-                );
-              },
-              child: Text("answer"),
+            Visibility(
+              visible: isShow,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.redAccent,
+                child: Column(
+                  children: [
+                    Text(
+                      "アンケートが届いています",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40),
+                      child: CustomButton(text: "回答する"),
+                    ),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ),
             ),
           ],
         ),
